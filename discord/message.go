c9325114f25ea79568db212437187874a4a045e8
apricotbucket28 <71973804+apricotbucket28@users.noreply.github.com>
2025-03-09 12:46:59 -0300

    Fix missing hostname from doc links (#437)

--- discord/message.go
+++ discord/message.go
@@ -214,7 +214,7 @@ const (
 	MessageReactionTypeBurst
 )
 
-// MessageActivityType is the type of MessageActivity https://com/developers/docs/resources/channel#message-object-message-activity-types
+// MessageActivityType is the type of MessageActivity https://discord.com/developers/docs/resources/message#message-object-message-activity-types
 type MessageActivityType int
 
 // Constants for MessageActivityType
