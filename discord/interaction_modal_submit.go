99a559c74a810b36979400e908693cc5b7226e45
apricotbucket28 <71973804+apricotbucket28@users.noreply.github.com>
2025-04-17 08:54:44 -0300

    Add `attachment_size_limit` field to interactions (#444)

--- discord/interaction_modal_submit.go
+++ discord/interaction_modal_submit.go
@@ -40,6 +40,7 @@ func (i *ModalSubmitInteraction) UnmarshalJSON(data []byte) error {
 	i.baseInteraction.entitlements = interaction.Entitlements
 	i.baseInteraction.authorizingIntegrationOwners = interaction.AuthorizingIntegrationOwners
 	i.baseInteraction.context = interaction.Context
+	i.baseInteraction.attachmentSizeLimit = interaction.AttachmentSizeLimit
 
 	if i.baseInteraction.member != nil && i.baseInteraction.guildID != nil {
 		i.baseInteraction.member.GuildID = *i.baseInteraction.guildID
@@ -72,6 +73,7 @@ func (i ModalSubmitInteraction) MarshalJSON() ([]byte, error) {
 			Entitlements:                 i.entitlements,
 			AuthorizingIntegrationOwners: i.authorizingIntegrationOwners,
 			Context:                      i.context,
+			AttachmentSizeLimit:          i.attachmentSizeLimit,
 		},
 		Data: i.Data,
 	})
